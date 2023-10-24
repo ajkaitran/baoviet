ab058555f3b7b5c213b279acabb46faec5b633e8
ajkaitran <trannamhai7626@gmail.com>
2023-10-24 10:59:30 +0700

    update

---
+++ code/app.js
@@ -0,0 +1,99 @@
+// Hiệu ứng
+AOS.init();
+// Slideshow
+$('.slide').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+});
+$('.slide_ld').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4, 
+    arrows: true,
+    prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+});
+$('.slide_2').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1, 
+    arrows: true,
+    prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 740,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+  ]
+  });
+$('.slide_4').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1, 
+    arrows: true,
+    prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: false
+          }
+        },
+        {
+          breakpoint: 740,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        }
+    ]
+});
+// Backtop
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop()) {
+            $('.backtop').fadeIn();
+        } else {
+            $('.backtop').fadeOut();
+        }
+    });
+    $(".backtop").click(function () {
+        $('html,body').animate({
+            scrollTop: 0
+        }, 500);
+    })
+});
