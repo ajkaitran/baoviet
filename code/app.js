e90fd2a48f9417b08611b6141377ae8a9d455116
ajkaitran <trannamhai7626@gmail.com>
2023-10-25 11:43:12 +0700

    update-code

--- code/app.js
+++ code/app.js
@@ -14,7 +14,32 @@ $('.slide_ld').slick({
     slidesToScroll: 4, 
     arrows: true,
     prevArrow: "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
-    nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>"
+    nextArrow: "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      },
+      {
+        breakpoint: 740,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+  ]
 });
 $('.slide_2').slick({
     infinite: true,
